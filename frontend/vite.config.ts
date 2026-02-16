a20a7cb734b454360796a41b32c71d9f57c154f4
Ignacio Ready <64984649+Ignacioready13@users.noreply.github.com>
2026-02-16 16:07:11 -0700

    idk

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -157,22 +157,6 @@ function getPlugins({
           applicationKey: "monkeytype-frontend",
         }) as Plugin)
       : null,
-    replace([
-      {
-        filter: ["src/ts/firebase.ts"],
-        replace: {
-          from: `"./constants/firebase-config.ts"`,
-          to: `"./constants/firebase-config-live.ts"`,
-        },
-      },
-      {
-        filter: ["src/email-handler.html"],
-        replace: {
-          from: `"./ts/constants/firebase-config"`,
-          to: `"./ts/constants/firebase-config-live"`,
-        },
-      },
-    ]),
     injectPreload(),
     minifyJson(),
   ];
